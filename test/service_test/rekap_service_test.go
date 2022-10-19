af21c8c5376f8e5ebd8c8a43e8b4dc4d478e7faa
faridlan <faridlannulhakim@gmail.com>
2022-10-19 22:16:21 +0700

    just joking

--- test/service_test/rekap_service_test.go
+++ test/service_test/rekap_service_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/faridlan/lans-app/model/domain"
 	"github.com/faridlan/lans-app/model/web"
 	"github.com/faridlan/lans-app/repository"
 	"github.com/faridlan/lans-app/service"
@@ -17,27 +18,30 @@ func TestCreate(t *testing.T) {
 	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
 	defer mt.Close()
 
-	mt.Run("success", func(t *mtest.T) {
+	mt.Run("success", func(mt *mtest.T) {
 		rekapCollection := *mt.Coll
 		rekapRepository := repository.NewRekapRepository(&rekapCollection)
-		rekapService := service.NewRekapService(rekapRepository)
-
+		rs := service.NewRekapService(rekapRepository)
+		// id := primitive.NewObjectID()
 		mt.AddMockResponses(mtest.CreateSuccessResponse())
 
-		response := rekapService.Create(context.Background(), web.RekapCreateRequest{
-			CsName:      "Jhon",
-			CusName:     "Udin",
-			RekapStatus: false,
+		resutl := rs.Create(context.Background(), web.RekapCreateRequest{
+			// Id:          id,
+			CsName:      "Udin",
+			CusName:     "Jhon",
+			RekapStatus: true,
 			PrintStatus: false,
 			RekapDate:   time.Now().Unix(),
 		})
 
-		assert.Equal(t, &web.RekapResponse{
-			CsName:      "Jhon",
-			CusName:     "Udin",
-			RekapStatus: false,
+		// assert.Nil(t, err)
+		assert.Equal(t, &domain.Rekap{
+			// Id:          resutl.Id,
+			CsName:      "Udin",
+			CusName:     "Jhon",
+			RekapStatus: true,
 			PrintStatus: false,
 			RekapDate:   time.Now().Unix(),
-		}, response)
+		}, resutl)
 	})
 }
